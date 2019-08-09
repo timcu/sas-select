6675447dc15f110d559bd474e2bb439c3c4c694d
Chayawit Wonganusorn <w.chayawit@gmail.com>
2019-08-09 12:46:07 +0700

    Remove AUTOINCREMENT from data_reading id

--- sas_select/schema_tbl_data_reading.sql
+++ sas_select/schema_tbl_data_reading.sql
@@ -1,7 +1,7 @@
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS tbl_data_reading;
 CREATE TABLE tbl_data_reading (
-    ID integer primary key autoincrement,
+    ID integer primary key,
     FileURL varchar,
     FileName varchar,
     ReadDate timestamp,
